e6e54dac4d8e5a0db37b6e76084cb6e6f8839607
Amanda McCann <amanda@technomancy.org>
2023-09-03 21:28:37 +0200

    + doccomments

--- src/nodeid_wayids.rs
+++ src/nodeid_wayids.rs
@@ -1,10 +1,13 @@
 //! A struct for storing which wayid(s) a nodeid is in.
-//! Most nodes are in 1 way. This struct uses much less memory by taking advantage of that.
+//! Most nodes are in only 1 way. This struct uses much less memory by taking advantage of that.
 use super::*;
 
 #[derive(Debug, GetSize)]
 pub struct NodeIdWayIds {
+    /// A node which is in exactly 1 way. Store the way id that it's in
     singles: HashMap<i64, i64>,
+
+    /// A node which is in many ways, Store the ways that it's in
     multiples: HashMap<i64, Vec<i64>>,
 }
 
@@ -16,6 +19,7 @@ impl NodeIdWayIds {
         }
     }
 
+    /// Record that node id `nid` is in way id `wid`.
     pub fn insert(&mut self, nid: i64, wid: i64) {
         if let Some(existing) = self.multiples.get_mut(&nid) {
             existing.push(wid);
@@ -33,13 +37,16 @@ impl NodeIdWayIds {
         }
     }
 
+    /// True iff node id `nid` has been seen
     pub fn contains_nid(&self, nid: &i64) -> bool {
         self.singles.contains_key(nid) || self.multiples.contains_key(nid)
     }
+    /// How many nodes have been saved
     pub fn len(&self) -> usize {
         self.singles.len() + self.multiples.len()
     }
 
+    /// Return all the ways that this node is in.
     pub fn ways<'a>(&'a self, nid: &i64) -> Box<dyn Iterator<Item = &i64> + 'a> {
         if let Some(wid) = self.singles.get(nid) {
             Box::new(std::iter::once(wid))
