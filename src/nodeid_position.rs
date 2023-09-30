1f8501ed45a4b8e43f8483f42954792423f93ed8
Amanda McCann <amanda@technomancy.org>
2023-09-30 11:39:54 +0200

    nodeid_pos: store as i32
    This saves about 33% of the space

--- src/nodeid_position.rs
+++ src/nodeid_position.rs
@@ -1,5 +1,6 @@
 use super::*;
-use std::collections::BTreeMap;
+use std::collections::{HashMap, BTreeMap};
+use osmio::{Lat, Lon};
 
 pub trait NodeIdPosition: std::fmt::Debug + std::marker::Send+std::marker::Sync {
     fn new() -> Self where Self: Sized;
@@ -28,8 +29,9 @@ pub(crate) fn default() -> impl NodeIdPosition {
 
 #[derive(Debug, GetSize)]
 pub struct NodeIdPositionMap {
-    inner: BTreeMap<i64, (f64, f64)>,
+    inner: BTreeMap<i64, (i32, i32)>,
 }
+
 
 impl NodeIdPosition for NodeIdPositionMap {
     fn new() -> Self {
@@ -37,15 +39,17 @@ impl NodeIdPosition for NodeIdPositionMap {
     }
 
     fn insert(&mut self, node_id: i64, pos: (f64, f64)) {
+        let pos = (Lat::try_from(pos.0).unwrap().inner(), Lon::try_from(pos.1).unwrap().inner());
         self.inner.insert(node_id, pos);
     }
 
     fn contains_key(&self, node_id: &i64) -> bool {
         self.inner.contains_key(node_id)
     }
+
     fn get(&self, node_id: &i64) -> Option<(f64, f64)> {
-        self.inner.get(node_id).copied()
-    }
+        self.inner.get(node_id).map(|(lat, lng)| (Lat::from_inner(*lat).degrees(), Lon::from_inner(*lng).degrees()))
+     }
 
     fn len(&self) -> usize {
         self.inner.len()
@@ -64,7 +68,7 @@ impl NodeIdPosition for NodeIdPositionMap {
 
     fn extend<I: IntoIterator<Item=(i64, (f64, f64))>>(&mut self, iter: I) {
         for el in iter {
-            self.insert(el.0, (el.1.0.try_into().unwrap(), el.1.1.try_into().unwrap()));
+            self.insert(el.0, ((el.1.0.try_into().unwrap(), el.1.1.try_into().unwrap())));
         }
     }
 
