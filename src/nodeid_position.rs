eafd1b6dbc515855ebdf5657ef3c8d77f52d234a
Amanda McCann <amanda@technomancy.org>
2023-10-01 10:12:52 +0200

    minor doccomments

--- src/nodeid_position.rs
+++ src/nodeid_position.rs
@@ -2,19 +2,27 @@ use super::*;
 use std::collections::{HashMap, BTreeMap};
 use osmio::{Lat, Lon};
 
-pub trait NodeIdPosition: std::fmt::Debug + std::marker::Send+std::marker::Sync {
-    fn new() -> Self where Self: Sized;
+pub trait NodeIdPosition: std::fmt::Debug + std::marker::Send + std::marker::Sync {
+    fn new() -> Self
+    where
+        Self: Sized;
 
+    /// Set this position
     fn insert(&mut self, node_id: i64, pos: (f64, f64));
 
-    fn contains_key(&self, node_id: &i64) -> bool;
+    fn contains_key(&self, node_id: &i64) -> bool {
+        self.get(node_id).is_some()
+    }
+    /// Return the location for this node id
     fn get(&self, node_id: &i64) -> Option<(f64, f64)>;
 
+    /// Number of nodes inside
     fn len(&self) -> usize;
 
+    /// Only keep nodeids which pass this function
     fn retain_by_key(&mut self, f: impl FnMut(&i64) -> bool);
 
-    fn extend<I: IntoIterator<Item=(i64, (f64, f64))>>(&mut self, iter: I) {
+    fn extend<I: IntoIterator<Item = (i64, (f64, f64))>>(&mut self, iter: I) {
         for el in iter {
             self.insert(el.0, el.1);
         }
@@ -36,14 +44,18 @@ pub struct NodeIdPositionMap {
     inner: BTreeMap<i64, (i32, i32)>,
 }
 
-
 impl NodeIdPosition for NodeIdPositionMap {
     fn new() -> Self {
-        NodeIdPositionMap{ inner: BTreeMap::new() }
+        NodeIdPositionMap {
+            inner: BTreeMap::new(),
+        }
     }
 
     fn insert(&mut self, node_id: i64, pos: (f64, f64)) {
-        let pos = (Lat::try_from(pos.0).unwrap().inner(), Lon::try_from(pos.1).unwrap().inner());
+        let pos = (
+            Lat::try_from(pos.0).unwrap().inner(),
+            Lon::try_from(pos.1).unwrap().inner(),
+        );
         self.inner.insert(node_id, pos);
     }
 
@@ -52,15 +64,19 @@ impl NodeIdPosition for NodeIdPositionMap {
     }
 
     fn get(&self, node_id: &i64) -> Option<(f64, f64)> {
-        self.inner.get(node_id).map(|(lat, lng)| (Lat::from_inner(*lat).degrees(), Lon::from_inner(*lng).degrees()))
-     }
+        self.inner.get(node_id).map(|(lat, lng)| {
+            (
+                Lat::from_inner(*lat).degrees(),
+                Lon::from_inner(*lng).degrees(),
+            )
+        })
+    }
 
     fn len(&self) -> usize {
         self.inner.len()
     }
 
-    fn retain_by_key(&mut self, mut f: impl FnMut(&i64) -> bool )
-    {
+    fn retain_by_key(&mut self, mut f: impl FnMut(&i64) -> bool) {
         self.inner.retain(|k, _v| f(k));
     }
 
@@ -75,12 +91,15 @@ impl NodeIdPosition for NodeIdPositionMap {
             self.get_size() as f64 / self.len() as f64,
         ));
         output
-
     }
 
-    fn extend<I: IntoIterator<Item=(i64, (f64, f64))>>(&mut self, iter: I) {
+    fn extend<I: IntoIterator<Item = (i64, (f64, f64))>>(&mut self, iter: I) {
         for el in iter {
-            self.insert(el.0, ((el.1.0.try_into().unwrap(), el.1.1.try_into().unwrap())));
+            self.insert(
+                el.0,
+                ((el.1 .0.try_into().unwrap(), el.1 .1.try_into().unwrap())),
+            );
+
         }
     }
 
