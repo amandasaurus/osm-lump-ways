d1b13afb81a1bb909c0bd445e0e92ccd7675f57c
Amanda McCann <amanda@technomancy.org>
2023-11-27 19:52:09 +0100

    refactor to reduce temp allocations

--- src/nodeid_position.rs
+++ src/nodeid_position.rs
@@ -182,10 +182,8 @@ impl NodeIdPositionBucket {
     fn write_out_cache(&mut self) {
         let bucket_shift = self.bucket_shift();
         if let Some(cache) = &mut self.cache {
-            // store the current cache in the inner (ie go from cache → real store )
-            let mut bytes = Vec::with_capacity(8 + 2 * cache.1.len());
-            bucket_bytes_write(bucket_shift, &cache.1, &mut bytes);
-            self.inner.insert(cache.0, bytes);
+            let mut inner_entry = self.inner.entry(cache.0).or_insert_with(|| Vec::with_capacity(8 + 2 * cache.1.len()));
+            bucket_bytes_write(bucket_shift, &cache.1, &mut inner_entry);
         }
     }
 
