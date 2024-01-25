bb337705469c2fd66fc86c3bc5ff7077cffdb201
Amanda McCann <amanda@technomancy.org>
2024-01-25 18:42:24 +0100

    calculate strahlers

--- src/bin/osm-lump-ways-down.rs
+++ src/bin/osm-lump-ways-down.rs
@@ -23,6 +23,8 @@ use num_format::{Locale, ToFormattedString};
 
 use country_boundaries::{CountryBoundaries, LatLon, BOUNDARIES_ODBL_360X180};
 
+use smallvec::SmallVec;
+
 #[path = "../cli_args.rs"]
 mod cli_args;
 #[path = "../haversine.rs"]
@@ -566,8 +568,38 @@ fn main() -> Result<()> {
     let (mut curr_upstream, mut num_outs, mut per_downstream, mut curr_pos);
     let (mut other_pos, mut this_edge_len);
 
+    // Vec<u32> → All upstream strahler numbers.
+    // Strahler https://en.wikipedia.org/wiki/Strahler_number needs to look at the parent, but we
+    // only have a single directed graph. So push the current strahler numbers down to the child
+    // nodes and then calculate the number when we get to that node.
+    // NB we use parent/child different order from Wikipedia
+    let mut parent_strahlers: BTreeMapSplitKey<SmallVec<[u32; 1]>> = BTreeMapSplitKey::new();
+
+    // final strahler numbers go here.
+    let mut strahler: BTreeMapSplitKey<u32> = BTreeMapSplitKey::new();
+
     for v in topologically_sorted_nodes.drain(..) {
         calc_upstream_bar.inc(1);
+
+        let mut this_strahler_value = 0;
+        assert!(!strahler.contains_key(&v));
+        if !parent_strahlers.contains_key(&v) {
+            this_strahler_value = 1;
+        } else {
+            let this_parent_strahlers = parent_strahlers.get(&v).unwrap();
+            let max_parent_strahler = this_parent_strahlers.iter().max().unwrap();
+            if this_parent_strahlers.iter().filter(|x| *x == max_parent_strahler).count() == 1 && this_parent_strahlers.iter().filter(|x| *x < max_parent_strahler).count() == this_parent_strahlers.len() - 1 {
+                this_strahler_value =  *max_parent_strahler;
+            } else if this_parent_strahlers.iter().filter(|x| *x == max_parent_strahler).count() >= 2 {
+                this_strahler_value = *max_parent_strahler+1;
+            } else {
+                dbg!(this_strahler_value, max_parent_strahler, v, this_parent_strahlers);
+                panic!();
+            }
+        }
+        *strahler.entry(v).or_default() = this_strahler_value;
+        // try to recoop some memory
+        parent_strahlers.remove(&v);
 
         curr_upstream = length_upstream.entry(v).or_insert(0.);
         num_outs = g.out_neighbours(v).count() as f64;
@@ -578,8 +610,13 @@ fn main() -> Result<()> {
             this_edge_len =
                 haversine::haversine_m(curr_pos.0, curr_pos.1, other_pos.0, other_pos.1);
             *length_upstream.entry(other).or_insert(0.) += per_downstream + this_edge_len;
+            parent_strahlers.entry(other).or_default().push(this_strahler_value);
+
         }
+
+
     }
+    drop(parent_strahlers);
     calc_upstream_bar.finish();
     progress_bars.remove(&calc_upstream_bar);
     info!(
@@ -588,7 +625,7 @@ fn main() -> Result<()> {
     );
     info_memory_used!();
 
-    debug!("Creating geojson object(s)");
+    debug!("Writing upstream geojson object(s)");
     let lines = g
         .edges_iter()
         .filter(|(from_nid, _to_nid)| {
@@ -634,6 +671,48 @@ fn main() -> Result<()> {
         args.output_filename.replace("%s", "upstreams")
     );
 
+
+    debug!("Writing strahler number geojson object(s)");
+    let strahler_lines = g
+        .edges_iter()
+        .map(|(from_nid, to_nid)| {
+            (
+                // Round the upstream to only output 1 decimal place
+                serde_json::json!({
+                    "strahler": strahler.get(&from_nid)
+                }),
+                vec![vec![
+                    nodeid_pos.get(&from_nid).unwrap(),
+                    nodeid_pos.get(&to_nid).unwrap(),
+                ]],
+            )
+        });
+    info_memory_used!();
+
+
+    let writing_upstreams_bar = progress_bars.add(
+        ProgressBar::new(g.num_edges() as u64)
+            .with_message("Writing strahler geojson(s) file")
+            .with_style(style.clone()),
+    );
+
+    let strahler_lines = strahler_lines.progress_with(writing_upstreams_bar);
+
+    let mut f = std::io::BufWriter::new(std::fs::File::create(
+        args.output_filename.replace("%s", "strahler"),
+    )?);
+    let num_strahler_written = write_geojson_features_directly(
+        strahler_lines,
+        &mut f,
+        &output_format,
+        &OutputGeometryType::LineString,
+    )?;
+    info!(
+        "Wrote {} features to output file {}",
+        num_strahler_written.to_formatted_string(&Locale::en),
+        args.output_filename.replace("%s", "strahler")
+    );
+
     let nids_wo_outgoing: BTreeSet<_> = g.vertexes_wo_outgoing_jumbled().collect();
     // look for where it ends
     let end_points = nids_wo_outgoing
@@ -657,12 +736,12 @@ fn main() -> Result<()> {
         &output_format,
         &OutputGeometryType::Point,
     )?;
-
     info!(
         "Wrote {} features to output file {}",
         num_written.to_formatted_string(&Locale::en),
         args.output_filename.replace("%s", "ends")
     );
+
 
     info!(
         "Finished all in {}",
