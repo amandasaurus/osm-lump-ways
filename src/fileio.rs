67bdad689770541065fcca191af54cfbae3df900
Amanda McCann <amanda@technomancy.org>
2024-03-10 22:24:36 +0100

    remove dead code

--- src/fileio.rs
+++ src/fileio.rs
@@ -134,34 +134,6 @@ fn write_point_coords(f: &mut impl Write, coords: &[Vec<(f64, f64)>]) -> Result<
     Ok(())
 }
 
-pub fn format_duration_human(duration: &std::time::Duration) -> String {
-    let sec_f = duration.as_secs_f32();
-    if sec_f < 60. {
-        let msec = (sec_f * 1000.).round() as u64;
-        if sec_f > 0. && msec == 0 {
-            "<1ms".to_string()
-        } else if msec > 0 && duration.as_secs_f32() < 1. {
-            format!("{}ms", msec)
-        } else {
-            format!("{:>3.1}s", sec_f)
-        }
-    } else {
-        let sec = sec_f.round() as u64;
-        let (min, sec) = (sec / 60, sec % 60);
-        if min < 60 {
-            format!("{}m{:02}s", min, sec)
-        } else {
-            let (hr, min) = (min / 60, min % 60);
-            if hr < 24 {
-                format!("{}h{:02}m{:02}s", hr, min, sec)
-            } else {
-                let (day, hr) = (hr / 24, hr % 24);
-                format!("{}d{:02}h{:02}m{:02}s", day, hr, min, sec)
-            }
-        }
-    }
-}
-
 fn write_linestring_coords(f: &mut impl Write, coords: &[Vec<(f64, f64)>]) -> Result<()> {
     f.write_all(b"[")?;
     for (j, j_coords) in coords[0].iter().enumerate() {
